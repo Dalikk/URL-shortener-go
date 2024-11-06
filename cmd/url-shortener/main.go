e60853ed48d014c950a408706e1e2f5f8781b9ec
dalikk <dmitriev.alik1999@gmail.com>
2024-11-07 00:32:31 +0900

    config: add general config

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"url-shortener/internal/config"
+)
+
 func main() {
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: slog
+
+	// TODO: run server
 
 }
